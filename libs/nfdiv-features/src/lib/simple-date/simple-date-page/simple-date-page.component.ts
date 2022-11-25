124898ad376c2bdc3cc223689ef205b9b864c742
Stephen Buckley <58293351+oimediaDev@users.noreply.github.com>
2022-11-25 13:25:05 +0000

    Add some styling to wrapping

--- libs/nfdiv-features/src/lib/simple-date/simple-date-page/simple-date-page.component.ts
+++ libs/nfdiv-features/src/lib/simple-date/simple-date-page/simple-date-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { SimpleDateComponent } from "@hmcts-common";
+import { SaveButtonGroupComponent, SimpleDateComponent } from "@hmcts-common";
 
 @Component({
   selector: "eui-simple-date-page",
   standalone: true,
-  imports: [CommonModule, SimpleDateComponent],
+  imports: [CommonModule, SimpleDateComponent, SaveButtonGroupComponent],
   templateUrl: "./simple-date-page.component.html",
   styleUrls: ["./simple-date-page.component.scss"],
 })
