1409f74936b0831dade9a7014debf4b238337663
Stephen Buckley <58293351+oimediaDev@users.noreply.github.com>
2022-11-30 15:30:15 +0000

    Rename Calender page

---
+++ libs/nfdiv-features/src/lib/appointment-calender-page/appointment-calendar-page.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AppointmentsModel } from "../appointment.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentCalendarPageService {
+  private appointment: AppointmentsModel | undefined
+  constructor() { }
+
+  setAppointment($event: AppointmentsModel) {
+    this.appointment = $event
+  }
+
+  getAppointment(){
+    return this.appointment;
+  }
+
+  updateAppointment(){
+
+  }
+}
