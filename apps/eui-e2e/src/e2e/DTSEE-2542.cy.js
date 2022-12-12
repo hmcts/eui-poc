b94f03e6a6027b027b950e2ff3f1546dbf0d53ce
Stephen Buckley <58293351+oimediaDev@users.noreply.github.com>
2022-12-12 08:05:44 +0000

    fix Cypress test

--- apps/eui-e2e/src/e2e/DTSEE-2542.cy.js
+++ apps/eui-e2e/src/e2e/DTSEE-2542.cy.js
@@ -1,15 +1,21 @@
 describe('empty spec', () => {
-  it('open and sets the viewport', () => {
+  beforeEach( ()=> {
     cy.viewport(1920,1024);
-    cy.visit('/microsite/nfdiv/case-details/1669996761194595/trigger/update-appointment')
+    cy.visit('http://localhost:4200/microsite/nfdiv/case-details/1669996761194595/trigger/update-appointment')
+
   })
   it('the header should be visible', () => {
     cy.get('.govuk-header__logotype-text').should('be.visible');
   })
   it('the case id should be visible', () => {
-    cy.get('.govuk-grid-column-two-thirds > :nth-child(1) > span').should('contain.text','1669-9967-61-4595');
+    cy.get('.govuk-grid-column-two-thirds > :nth-child(1) > span').should('contain.text','Your Case Number is 1669-9967-6119-4595');
   })
   it('the continue button should be disabled', () => {
     cy.get('[data-module="govuk-button"]').should('be.disabled');
+  })
+  it('should allow the user to select todays date', () => {
+    cy.get('.mat-calendar-body-today').should('be.visible');
+    cy.get('.mat-calendar-body-today').click()
+    cy.get('.mat-calendar-body-selected').should('be.visible');
   })
 })
--- apps/eui-e2e/src/e2e/home/home.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('eui', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=homecomponent--primary'));
-  it('should render the component', () => {
-    cy.get('eui-home').should('exist');
-  });
-});
