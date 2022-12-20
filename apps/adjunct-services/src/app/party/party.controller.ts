bf4046a39e477457849e00547bc83d8a24e5862b
Stephen Buckley <58293351+oimediaDev@users.noreply.github.com>
2022-12-20 19:01:31 +0000

    TODO -- URL filtering not working

--- apps/adjunct-services/src/app/party/party.controller.ts
+++ apps/adjunct-services/src/app/party/party.controller.ts
@@ -1,37 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { PartyService } from './party.service';
-import { CreatePartyDto } from './dto/create-party.dto';
-import { UpdatePartyDto } from './dto/update-party.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { PartyService } from "./party.service";
+import { CreatePartyDto } from "./dto/create-party.dto";
+import { UpdatePartyDto } from "./dto/update-party.dto";
 
-@Controller('party')
+@Controller("party")
 export class PartyController {
   constructor(private readonly partyService: PartyService) {}
 
-  @Post()
-  async create(@Body() body, createPartyDto: CreatePartyDto) {
-    console.log(body)
-    if (createPartyDto === undefined) {
-      createPartyDto = new CreatePartyDto()
-    }
-    createPartyDto.connectedCases = body.caseId;
-    createPartyDto.firstName = body.item.firstName
-    createPartyDto.lastName = body.item.lastName
-    let reponse = await this.partyService.create(createPartyDto);
-    return reponse
-  }
-
   @Get()
-  findAll() {
-    return this.partyService.findAll();
+  findAll(@Param('caseId') id: string) {
+    console.log("Call to find All");
+    if (id) {
+      return this.partyService.findAllByCaseId(id);
+    } else {
+      return this.partyService.findAll();
+    }
   }
-
-  @Get(':caseId')
+  @Get('/byCaseId:caseId')
   findMany(@Param('caseId') id: string) {
+    console.log("Call to find Many");
     return this.partyService.findAllByCaseId(id);
   }
-  @Get(':id')
+  @Get('?id')
   findOne(@Param('id') id: string) {
     return this.partyService.findOne(+id);
+  }
+  @Post()
+  async create(@Body() body, createPartyDto: CreatePartyDto) {
+    if (createPartyDto === undefined) {
+      createPartyDto = new CreatePartyDto();
+    }
+    createPartyDto.connectedCases = body.caseId;
+    createPartyDto.firstName = body.item.firstName;
+    createPartyDto.lastName = body.item.lastName;
+    let reponse = await this.partyService.create(createPartyDto);
+    return reponse;
   }
 
   @Patch(':id')
