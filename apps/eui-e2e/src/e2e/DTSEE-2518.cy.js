725a6c6162bbf7a5e4f898bf5f8b1dea78c9e933
Stephen Buckley <58293351+oimediaDev@users.noreply.github.com>
2023-01-04 12:18:49 +0000

    Add, Edit Delete a Party

--- apps/eui-e2e/src/e2e/DTSEE-2518.cy.js
+++ apps/eui-e2e/src/e2e/DTSEE-2518.cy.js
@@ -8,6 +8,15 @@ describe("Testing the Multi Party page -- Direct selection method", () => {
     cy.intercept("GET", "/microsite/nfdiv/api/party*", (req) => {
       req.reply({ fixture: "getParties.json" });
     }).as("parties");
+    cy.intercept("PATCH", "/microsite/nfdiv/api/party/6", (req) => {
+      req.reply({ fixture: "getPartiesPatch.json" });
+    }).as("partiesPatch");
+    cy.intercept("POST", "/microsite/nfdiv/api/party", (req) => {
+      req.reply({ fixture: "getPartiesAddOne.json" });
+    }).as("partiesAddOne");
+    cy.intercept("DELETE", "/microsite/nfdiv/api/party/6", (req) => {
+      req.reply({ fixture: "getPartiesDeleteOne.json" });
+    }).as("partiesDelete");
     cy.visit(
       `${baseUrl}/microsite/nfdiv/case-details/1669996761194595/trigger/update-parties`
     );
@@ -36,10 +45,9 @@ describe("Testing the Multi Party page -- Direct selection method", () => {
     );
   });
 
-
   it("The parties list should contain six items", () => {
     cy.wait("@parties");
-    cy.get('[data-cy="party-list"]>').should("have.length", 6);
+    cy.get('[data-test="party-list"]>').should("have.length", 6);
   });
 
   it("It should carry out a case insensitive filter", () => {
@@ -54,21 +62,61 @@ describe("Testing the Multi Party page -- Direct selection method", () => {
     cy.get("#mat-input-0").clear("f");
     cy.get("#mat-input-0").type("fred");
     cy.get("mat-selection-list >").should("have.length", 3);
-    cy.get('.mdc-switch__icon--off').click();
+    cy.get(".mdc-switch__icon--off").click();
     cy.get("mat-selection-list >").should("have.length", 1);
-
   });
 
-  it ('should clear the filter', () => {
+  it("should clear the filter", () => {
     cy.get("#mat-mdc-form-field-label-0 > .ng-tns-c120-0").click();
     cy.get("#mat-input-0").clear("f");
     cy.get("#mat-input-0").type("fred");
     cy.get("mat-selection-list >").should("have.length", 3);
-    cy.get('.mdc-switch__icon--off').click();
+    cy.get(".mdc-switch__icon--off").click();
     cy.get("mat-selection-list >").should("have.length", 1);
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('.mat-mdc-button-touch-target').click();
+    cy.get(".mat-mdc-button-touch-target").click();
     /* ==== End Cypress Studio ==== */
     cy.get("mat-selection-list >").should("have.length", 6);
-  })
+    /* ==== Generated with Cypress Studio ==== */
+  });
+
+  it("should edit a user", () => {
+    cy.wait("@parties");
+    cy.get("mat-selection-list >").should("have.length", 6);
+    cy.get("mat-selection-list  > :nth-child(6) ").click();
+    cy.get('[data-test="edit-button"]').click();
+    cy.get("#first-name").clear();
+    cy.get("#first-name").type("Edmund");
+    cy.get("#last-name").clear();
+    cy.get("#last-name").type("Sigfredson");
+    cy.get(".edit-form > :nth-child(2)").click();
+    cy.wait("@partiesPatch");
+    /* ==== End Cypress Studio ==== */
+    cy.get("mat-selection-list >").should("have.length", 6);
+    cy.get("mat-selection-list  > :nth-child(6) ").should(
+      "contain.text",
+      "Sigfredson"
+    );
+  });
+  it("should add a user", () => {
+    cy.wait("@parties");
+    cy.get("mat-selection-list >").should("have.length", 6);
+    cy.get('[data-test="add-button"]').click();
+    cy.get("#first-name").clear();
+    cy.get("#first-name").type("Edmund");
+    cy.get("#last-name").clear();
+    cy.get("#last-name").type("Amunsen");
+    cy.get(".edit-form > button").click();
+    cy.wait("@partiesAddOne");
+    cy.get("mat-selection-list >").should("have.length", 7);
+  });
+
+  it("should delete a user", () => {
+    cy.wait("@parties");
+    cy.get("mat-selection-list >").should("have.length", 6);
+    cy.get("mat-selection-list  > :nth-child(6) ").click();
+    cy.get('[data-test="delete-button"]').click();
+    cy.wait("@partiesDelete");
+    cy.get("mat-selection-list >").should("have.length", 5);
+  });
 });
